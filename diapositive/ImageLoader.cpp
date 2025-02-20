202aec3ba57eb0e22a8b739dc4e535ffcb31e2e3
ciabatti <lorenzo.ciabatti@edu.unifi.it>
2025-02-20 11:35:03 +0100

    riorganizzazione delle classi e modifica del dp observer

---
+++ diapositive/ImageLoader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lorenzo Ciabatti on 20/02/25.
+//
+
+#include "ImageLoader.h"
---
+++ diapositive/ImageLoader.h
@@ -0,0 +1,16 @@
+//
+// Created by Lorenzo Ciabatti on 20/02/25.
+//
+
+#ifndef IMAGELOADER_H
+#define IMAGELOADER_H
+
+
+
+class ImageLoader {
+
+};
+
+
+
+#endif //IMAGELOADER_H
